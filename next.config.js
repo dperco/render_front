847aee85925153266a77315dbfeb937930159fcb
Daniel Perco <daniel.perco@mindfactory.ar>
2025-08-09 20:25:48 -0300

    arreglando buils5

--- next.config.js
+++ next.config.js
@@ -1,15 +1,13 @@
 module.exports = {
-  // Habilita el modo standalone para mejor compatibilidad con Netlify
   output: 'standalone',
-  // Configuración para ignorar errores durante el build
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
-  // Configuración para manejar fallos en SSG
   experimental: {
-    missingSuspenseWithCSRBailout: false
+    serverActions: true,
+    optimizePackageImports: ['@mui/material']
   }
 }
